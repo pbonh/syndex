ec7a32674a6fa39f6d8ec488908ba887fc20a798
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2024-05-27 20:16:45 +0000

    [Program] More demorgans test scenarios.

--- src/engine/program_builder.rs
+++ src/engine/program_builder.rs
@@ -31,7 +31,7 @@ mod tests {
     use llhd::table::TableKey;
 
     #[test]
-    fn rewrite_llhd_inst_via_ascent_engine() {
+    fn rewrite_llhd_inst_via_ascent_engine_2and_1or() {
         let input = indoc::indoc! {"
                 entity @test_entity (i1 %in1, i1 %in2, i1 %in3, i1 %in4) -> (i1$ %out1) {
                     %null = const time 0s 1e
@@ -122,7 +122,99 @@ mod tests {
     }
 
     #[test]
-    fn rewrite_llhd_inst_via_ascent_engine_no_op() {
+    fn rewrite_llhd_inst_via_ascent_engine_2or_1and() {
+        let input = indoc::indoc! {"
+                entity @test_entity (i1 %in1, i1 %in2, i1 %in3, i1 %in4) -> (i1$ %out1) {
+                    %null = const time 0s 1e
+                    %or1 = or i1 %in1, %in2
+                    %or2 = or i1 %in3, %in4
+                    %and1 = and i1 %or1, %or2
+                    drv i1$ %out1, %and1, %null
+                }
+            "};
+
+        let module = llhd::assembly::parse_module(input).unwrap();
+        let llhd_world = LLHDWorld::new(LLHDModule::from(module));
+        let unit_id = UnitId::new(0);
+        let unit_program_and_inst: Vec<(Value, Value, Value, u64)> = llhd_world
+            .unit_program_inst::<LLHDInstComponent>(unit_id)
+            .filter(|((_unit_id, _inst_id), inst_component)| {
+                Opcode::And == inst_component.data.opcode()
+            })
+            .map(|(_inst_idx, inst_component)| {
+                let inst_value = inst_component.value.unwrap();
+                let args = inst_component.data.args();
+                let input1 = args[0];
+                let input2 = args[1];
+                (inst_value, input1, input2, 2)
+            })
+            .collect();
+        let unit_program_or_inst: Vec<(Value, Value, Value, u64)> = llhd_world
+            .unit_program_inst::<LLHDInstComponent>(unit_id)
+            .filter(|((_unit_id, _inst_id), inst_component)| {
+                Opcode::Or == inst_component.data.opcode()
+            })
+            .map(|(_inst_idx, inst_component)| {
+                let inst_value = inst_component.value.unwrap();
+                let args = inst_component.data.args();
+                let input1 = args[0];
+                let input2 = args[1];
+                (inst_value, input1, input2, 5)
+            })
+            .collect();
+
+        assert_eq!(
+            1,
+            unit_program_and_inst.len(),
+            "There should be 1 And instructions before program optimization."
+        );
+        assert_eq!(
+            2,
+            unit_program_or_inst.len(),
+            "There should be 2 Or instruction before program optimization."
+        );
+        let mut area_before = 0;
+        for inst in &unit_program_and_inst {
+            area_before += inst.3;
+        }
+        for inst in &unit_program_or_inst {
+            area_before += inst.3;
+        }
+        assert_eq!(
+            12, area_before,
+            "Area-Before should be 9, with 2 Or's and 1 And."
+        );
+        let mut prog = AscentProgram {
+            andi: unit_program_and_inst,
+            ori: unit_program_or_inst,
+            ..Default::default()
+        };
+        assert_eq!(
+            1,
+            prog.andi.len(),
+            "There should be 1 And instructions before running the engine."
+        );
+        assert_eq!(
+            2,
+            prog.ori.len(),
+            "There should be 2 And instructions before running the engine."
+        );
+        prog.run();
+        assert_eq!(
+            3,
+            prog.andi.len(),
+            "There should be 3 And instructions after running the engine."
+        );
+        assert_eq!(
+            3,
+            prog.ori.len(),
+            "There should be 3 And instructions after running the engine."
+        );
+    }
+
+
+    #[test]
+    fn rewrite_llhd_inst_via_ascent_engine_no_op_2or_1and() {
         let input = indoc::indoc! {"
                 entity @test_entity (i1 %in1, i1 %in2, i1 %in3, i1 %in4) -> (i1$ %out1) {
                     %null = const time 0s 1e
@@ -209,6 +301,97 @@ mod tests {
             2,
             prog.ori.len(),
             "There should be 2 And instructions after running the engine."
+        );
+    }
+
+    #[test]
+    fn rewrite_llhd_inst_via_ascent_engine_no_op_2and_1or() {
+        let input = indoc::indoc! {"
+                entity @test_entity (i1 %in1, i1 %in2, i1 %in3, i1 %in4) -> (i1$ %out1) {
+                    %null = const time 0s 1e
+                    %and1 = and i1 %in1, %in2
+                    %and2 = and i1 %in3, %in4
+                    %or1 = or i1 %and1, %and2
+                    drv i1$ %out1, %or1, %null
+                }
+            "};
+
+        let module = llhd::assembly::parse_module(input).unwrap();
+        let llhd_world = LLHDWorld::new(LLHDModule::from(module));
+        let unit_id = UnitId::new(0);
+        let unit_program_and_inst: Vec<(Value, Value, Value, u64)> = llhd_world
+            .unit_program_inst::<LLHDInstComponent>(unit_id)
+            .filter(|((_unit_id, _inst_id), inst_component)| {
+                Opcode::And == inst_component.data.opcode()
+            })
+            .map(|(_inst_idx, inst_component)| {
+                let inst_value = inst_component.value.unwrap();
+                let args = inst_component.data.args();
+                let input1 = args[0];
+                let input2 = args[1];
+                (inst_value, input1, input2, 2)
+            })
+            .collect();
+        let unit_program_or_inst: Vec<(Value, Value, Value, u64)> = llhd_world
+            .unit_program_inst::<LLHDInstComponent>(unit_id)
+            .filter(|((_unit_id, _inst_id), inst_component)| {
+                Opcode::Or == inst_component.data.opcode()
+            })
+            .map(|(_inst_idx, inst_component)| {
+                let inst_value = inst_component.value.unwrap();
+                let args = inst_component.data.args();
+                let input1 = args[0];
+                let input2 = args[1];
+                (inst_value, input1, input2, 5)
+            })
+            .collect();
+
+        assert_eq!(
+            2,
+            unit_program_and_inst.len(),
+            "There should be 2 And instructions before program optimization."
+        );
+        assert_eq!(
+            1,
+            unit_program_or_inst.len(),
+            "There should be 1 Or instruction before program optimization."
+        );
+        let mut area_before = 0;
+        for inst in &unit_program_and_inst {
+            area_before += inst.3;
+        }
+        for inst in &unit_program_or_inst {
+            area_before += inst.3;
+        }
+        assert_eq!(
+            9, area_before,
+            "Area-Before should be 12, with 2 And's and 1 Or."
+        );
+        let mut prog = AscentProgram {
+            andi: unit_program_and_inst,
+            ori: unit_program_or_inst,
+            ..Default::default()
+        };
+        assert_eq!(
+            2,
+            prog.andi.len(),
+            "There should be 2 And instructions before running the engine."
+        );
+        assert_eq!(
+            1,
+            prog.ori.len(),
+            "There should be 1 Or instructions before running the engine."
+        );
+        prog.run();
+        assert_eq!(
+            2,
+            prog.andi.len(),
+            "There should be 2 And instructions after running the engine."
+        );
+        assert_eq!(
+            1,
+            prog.ori.len(),
+            "There should be 1 Or instructions after running the engine."
         );
     }
 }
