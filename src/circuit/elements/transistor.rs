abe07726839975875e1190cc55655aed75ef18c5
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2024-07-09 02:26:55 +0000

    [Circuit] Circuit elements should be able to implement default.

--- src/circuit/elements/transistor.rs
+++ src/circuit/elements/transistor.rs
@@ -2,7 +2,7 @@ use crate::circuit::equations::CircuitEquation;
 use crate::circuit::nodes::CircuitNode;
 use evalexpr::HashMapContext;
 
-#[derive(Debug,Clone)]
+#[derive(Debug,Clone,Default)]
 pub struct Transistor {
     context: HashMapContext,
     equations: CircuitEquation,
