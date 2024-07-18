8017e0880a6628ee033a48010ba951904bc630ba
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2024-07-18 09:42:45 -0700

    [Repo] Remove flecs dependency, add protobuf system dependency via nix.

--- src/llhd_world.rs
+++ src/llhd_world.rs
@@ -1,6 +1,6 @@
 mod bevy_example;
 pub(crate) mod components;
-mod example_hierarchy;
-mod example_relations;
+// mod example_hierarchy;
+// mod example_relations;
 mod initializer;
 pub(crate) mod world;
