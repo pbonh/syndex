01859964988401d978179178e207b34d741b3ac7
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2024-07-19 02:55:24 +0000

    [Circuit] IN-PROGRESS More parser improvements.

--- src/llhd_library/circuit_library.rs
+++ src/llhd_library/circuit_library.rs
@@ -48,23 +48,43 @@ use peginator_macro::peginate;
 peginate!(
     "
 @export
-SPICENetlist = { elements:Elements };
-Elements = Resistor
-            | Capacitor
-            | Inductor
-            | MutualInductor
-            | VoltageControlledSwitch
-            | VoltageSource
-            | CurrentSource
-            | VoltageControlledVoltageSource
-            | VoltageControlledCurrentSource
-            | CurrentControlledCurrentSource
-            | Diode
-            | MOSTransistor;
+SPICENetlist = { statements:Statement };
+Statement    = comments:Comments | elements:Elements | commands:Command;
+Elements     = Resistor
+                | Capacitor
+                | Inductor
+                | MutualInductor
+                | VoltageControlledSwitch
+                | VoltageSource
+                | CurrentSource
+                | VoltageControlledVoltageSource
+                | VoltageControlledCurrentSource
+                | CurrentControlledCurrentSource
+                | Diode
+                | MOSTransistor;
+
+@no_skip_ws
+Comments         = '*' {!'\n' char} '\n';
+
+Command          = '.' option:Option
+                        | transient:Transient
+                        | print:Print
+                        | plot:Plot
+                        | end:End;
+
+Option           = i'options' { OptionArguments };
+Transient        = i'tran' { OptionArguments };
+Print            = i'print' { OptionArguments };
+Plot             = i'plot' { OptionArguments };
+OptionArguments  = modelId:Identifier | value:OptionExpression | assignment:OptionAssignment;
+OptionAssignment = Identifier '=' { value:OptionValue };
+OptionExpression = Identifier '(' { value:OptionValue } ')';
+OptionValue      =  UnitValue | Identifier ;
+End              = 'end';
 
 Resistor        = i'R' Identifier Node Node Value;
 Capacitor       = i'C' Identifier Node Node Value [ i'ic=' Value ];
-Inductor        = i'L' Identifier Node Node Value [ i'ic=' Float ];
+Inductor        = i'L' Identifier Node Node Value [ i'ic=' Value ];
 MutualInductor  = i'K' Identifier Identifier Identifier ( Value | i'k=' Value );
 VoltageControlledSwitch =
                     i'S' Identifier Node Node Node Node ModelId;
@@ -77,16 +97,17 @@ VoltageControlledCurrentSource =
 CurrentControlledCurrentSource =
                     i'F' Identifier Node Node Identifier Value;
 Diode           = i'D' Identifier Node Node ModelId { DiodeParam };
-MOSTransistor   = i'M' Identifier Node Node Node Node ModelId i'w=' Float i'l=' Float;
+MOSTransistor   = i'M' Identifier Node Node Node Node ModelId i'w=' Num i'l=' Num;
 
+Node            = Identifier;
 Identifier      = Letter { Letter | Digit };
-Node            = Letter { Letter | Digit };
-Value           = Float;
-TypeValue       = i'type=' TypeIdentifier TypeIdentifier '=' Float;
+Value           = UnitValue;
+TypeValue       = i'type=' TypeIdentifier TypeIdentifier '=' Num;
 TypeIdentifier  = 'vdc' | 'vac' | 'idc' | 'iac';
-DiodeParam      = ( 'AREA=' Float | 'T=' Float | 'IC=' Float | 'OFF=' Boolean );
+DiodeParam      = ( 'AREA=' Num | 'T=' Num | 'IC=' Num | 'OFF=' Boolean );
 ModelId         = Identifier;
-Float           = Digit { Digit } [ '.' { Digit } ];
+Num           = Digit [ Digit ] [ '.' { Digit } ];
+UnitValue       = Digit [ Digit ] [ '.' { Digit } ] [ Letter ];
 @string
 Letter          = 'a'..'z' | 'A'..'Z';
 @string
@@ -110,7 +131,12 @@ mod tests {
         let mut spice_netlist_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         spice_netlist_path.push("resources/spice3f5_examples/mosamp2.cir");
         let spice_netlist_str: String = fs::read_to_string(spice_netlist_path).unwrap();
-        let result = SPICENetlist::parse(&spice_netlist_str).unwrap();
-        println!("{:?}", result.elements);
+        let ast = SPICENetlist::parse(&spice_netlist_str).unwrap();
+        println!("Comments: {:?}", ast.comments);
+        println!("Commands: {:?}", ast.commands);
+        println!("Elements: {:?}", ast.elements);
+        assert_eq!(1, ast.comments.len(), "There should be 1 Comment in netlist.");
+        assert_eq!(5, ast.statements.len(), "There should be 5 Statements in netlist.");
+        assert_eq!(33, ast.elements.len(), "There should be 33 Elements in netlist.");
     }
 }
