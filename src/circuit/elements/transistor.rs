7dd479281ebac5e9176848e21f6bab25df7c2d0f
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2024-07-26 17:56:10 +0000

    [Circuit] Surround node differences in parantheses.

--- src/circuit/elements/transistor.rs
+++ src/circuit/elements/transistor.rs
@@ -34,18 +34,21 @@ impl Transistor {
         DeviceEquation,
         DeviceEquation,
     ) {
-        let vgs_str = gate.to_string() + "-" + &source.to_string();
-        let vgd_str = gate.to_string() + "-" + &drain.to_string();
-        let vgb_str = gate.to_string() + "-" + &body.to_string();
-        let vds_str = drain.to_string() + "-" + &source.to_string();
-        let vdb_str = drain.to_string() + "-" + &body.to_string();
-        let vsb_str = source.to_string() + "-" + &body.to_string();
-        let vgs = DeviceEquation::from_str(&vgs_str).expect("Invalid resulting equation");
-        let vgd = DeviceEquation::from_str(&vgd_str).expect("Invalid resulting equation");
-        let vgb = DeviceEquation::from_str(&vgb_str).expect("Invalid resulting equation");
-        let vds = DeviceEquation::from_str(&vds_str).expect("Invalid resulting equation");
-        let vdb = DeviceEquation::from_str(&vdb_str).expect("Invalid resulting equation");
-        let vsb = DeviceEquation::from_str(&vsb_str).expect("Invalid resulting equation");
+        let build_node_difference_str = |node1: &str, node2: &str| {
+            let mut node_difference_str = "(".to_owned();
+            node_difference_str.push_str(node1);
+            node_difference_str.push('-');
+            node_difference_str.push_str(node2);
+            node_difference_str.push(')');
+            DeviceEquation::from_str(&node_difference_str).expect("Invalid resulting equation")
+        };
+
+        let vgs = build_node_difference_str(&gate.to_string(), &source.to_string());
+        let vgd = build_node_difference_str(&gate.to_string(), &drain.to_string());
+        let vgb = build_node_difference_str(&gate.to_string(), &body.to_string());
+        let vds = build_node_difference_str(&drain.to_string(), &source.to_string());
+        let vdb = build_node_difference_str(&drain.to_string(), &body.to_string());
+        let vsb = build_node_difference_str(&source.to_string(), &body.to_string());
         (vgs, vgd, vgb, vds, vdb, vsb)
     }
 }
@@ -155,12 +158,12 @@ mod tests {
             .expect("Failure to convert `SPICENetlist` `Node` to `CircuitNode`");
         let (vgs, vgd, vgb, vds, vdb, vsb) =
             Transistor::transistor_node_subst(&source, &drain, &gate, &body);
-        assert_eq!("n3-n1", vgs.to_string());
-        assert_eq!("n3-n2", vgd.to_string());
-        assert_eq!("n3-n4", vgb.to_string());
-        assert_eq!("n2-n1", vds.to_string());
-        assert_eq!("n2-n4", vdb.to_string());
-        assert_eq!("n1-n4", vsb.to_string());
+        assert_eq!("(n3-n1)", vgs.to_string());
+        assert_eq!("(n3-n2)", vgd.to_string());
+        assert_eq!("(n3-n4)", vgb.to_string());
+        assert_eq!("(n2-n1)", vds.to_string());
+        assert_eq!("(n2-n4)", vdb.to_string());
+        assert_eq!("(n1-n4)", vsb.to_string());
     }
 
     #[test]
@@ -175,12 +178,12 @@ mod tests {
             .expect("Failure to convert `SPICENetlist` `Node` to `CircuitNode`");
         let (vgs, vgd, vgb, vds, vdb, vsb) =
             Transistor::transistor_node_subst(&source, &drain, &gate, &body);
-        assert_eq!("v_gate-v_source", vgs.to_string());
-        assert_eq!("v_gate-v_drain", vgd.to_string());
-        assert_eq!("v_gate-v_body", vgb.to_string());
-        assert_eq!("v_drain-v_source", vds.to_string());
-        assert_eq!("v_drain-v_body", vdb.to_string());
-        assert_eq!("v_source-v_body", vsb.to_string());
+        assert_eq!("(v_gate-v_source)", vgs.to_string());
+        assert_eq!("(v_gate-v_drain)", vgd.to_string());
+        assert_eq!("(v_gate-v_body)", vgb.to_string());
+        assert_eq!("(v_drain-v_source)", vds.to_string());
+        assert_eq!("(v_drain-v_body)", vdb.to_string());
+        assert_eq!("(v_source-v_body)", vsb.to_string());
 
         let mut spice_netlist_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         spice_netlist_path.push("resources/spice3f5_examples/mosamp2.cir");
@@ -204,7 +207,7 @@ mod tests {
             Is = 1e-12;
             eta = 1.5;
             Vt = T/11586;
-            I = Is*(e^(5-15/(eta*Vt)) - 1)
+            I = Is*(e^((5-15)/(eta*Vt)) - 1)
         "};
         assert_eq!(eq_expected, xtor.equations.to_string());
     }
