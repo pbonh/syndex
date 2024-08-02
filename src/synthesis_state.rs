ec2e7f94c2b51b52a7c31abbe1c92d8b9ec8dfa4
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2024-08-02 21:53:42 +0000

    [Synthesis] Add functional programming laws.

--- src/synthesis_state.rs
+++ src/synthesis_state.rs
@@ -2,6 +2,46 @@ mod typestate_doc_example;
 
 use typestate::typestate;
 
+/// Functor Typeclass
+///
+/// class Functor f where
+/// fmap :: (a -> b) -> f a -> f b
+///
+/// Functor Laws
+///
+/// fmap id = id                   -- 1st functor law
+/// fmap (g . f) = fmap g . fmap f -- 2nd functor law
+///
+/// -----------------------------------------------------------------------------------------------
+///
+/// Applicative Typeclass
+///
+/// class (Functor f) => Applicative f where
+/// pure  :: a -> f a
+/// (<*>) :: f (a -> b) -> f a -> f b
+///
+/// Applicative Laws
+///
+/// pure id <*> v = v                            -- Identity
+/// pure f <*> pure x = pure (f x)               -- Homomorphism
+/// u <*> pure y = pure ($ y) <*> u              -- Interchange
+/// pure (.) <*> u <*> v <*> w = u <*> (v <*> w) -- Composition
+///
+/// -----------------------------------------------------------------------------------------------
+///
+/// Monad Typeclass
+///
+/// class Monad m where
+/// (>>=)  :: m a -> (  a -> m b) -> m b
+/// (>>)   :: m a ->  m b         -> m b
+/// return ::   a                 -> m a
+///
+/// Monad Laws
+///
+/// return a >>= k                  =  k a
+/// m        >>= return             =  m
+/// m        >>= (\x -> k x >>= h)  =  (m >>= k) >>= h
+///
 #[typestate]
 pub mod builder {
     use crate::llhd::module::LLHDModule;
