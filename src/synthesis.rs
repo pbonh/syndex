3529958c13c90a5ef877c9e1eec72ad2afdcecf8
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2024-08-20 23:59:29 +0000

    [LLHD,Egglog] Add LLHD divisor extraction example.

--- src/synthesis.rs
+++ src/synthesis.rs
@@ -1,9 +1,12 @@
 use egglog::*;
 
-fn build_egraph(program: &str) -> EGraph {
+fn build_egraph(program: &str) -> (EGraph, Vec<String>) {
     let mut egraph = EGraph::default();
-    let _ = egraph.parse_and_run_program(program);
-    egraph
+    // egraph.run_mode = RunMode::ShowDesugaredEgglog;
+    let msgs = egraph
+        .parse_and_run_program(program)
+        .expect("Failure to run program on egraph.");
+    (egraph, msgs)
 }
 
 #[cfg(test)]
@@ -18,7 +21,8 @@ mod tests {
         let mut egglog_file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         egglog_file_path.push("resources/egglog/math_example.egg");
         let egglog_program: String = fs::read_to_string(egglog_file_path).unwrap();
-        let egraph = build_egraph(&egglog_program);
+        let egraph_info = build_egraph(&egglog_program);
+        let egraph = egraph_info.0;
         assert_eq!(
             1578,
             egraph.num_tuples(),
@@ -26,16 +30,43 @@ mod tests {
         );
     }
 
-    #[test]
+    #[test_log::test]
     fn build_egraph_with_string_bdd_example() {
         let mut egglog_file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         egglog_file_path.push("resources/egglog/bdd_example.egg");
         let egglog_program: String = fs::read_to_string(egglog_file_path).unwrap();
-        let egraph = build_egraph(&egglog_program);
+        let egraph_info = build_egraph(&egglog_program);
+        let egraph = egraph_info.0;
+        let egraph_msgs = egraph_info.1;
         assert_eq!(
             4,
             egraph.num_tuples(),
             "There should be 4 facts remaining in the egraph."
+        );
+        println!("{:?}", egraph_msgs);
+    }
+
+    #[test]
+    fn build_egraph_with_string_llhd_example() {
+        let mut egglog_file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        egglog_file_path.push("resources/egglog/llhd_dfg.egg");
+        let egglog_program: String = fs::read_to_string(egglog_file_path).unwrap();
+        let egraph_info = build_egraph(&egglog_program);
+        let egraph = egraph_info.0;
+        let run_report_matches = egraph
+            .get_run_report()
+            .clone()
+            .unwrap()
+            .num_matches_per_rule
+            .len();
+        assert_eq!(
+            1, run_report_matches,
+            "There should be 1 rule match in program."
+        );
+        assert_eq!(
+            27,
+            egraph.num_tuples(),
+            "There should be 27 facts remaining in the egraph."
         );
     }
 }
