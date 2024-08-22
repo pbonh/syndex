55f4c49abed6a33f4377fa7a710af78c7ec06d7f
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2024-08-22 06:07:33 +0000

    [LLHD,Egglog] Add llhd rewrite example with nested expression tree.

--- src/synthesis.rs
+++ src/synthesis.rs
@@ -94,4 +94,28 @@ mod tests {
             "There should be 27 facts remaining in the egraph."
         );
     }
+
+    #[test]
+    fn build_egraph_with_string_llhd_nested_expr() {
+        let mut egglog_file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        egglog_file_path.push("resources/egglog/llhd_dfg_nested_expr.egg");
+        let egglog_program: String = fs::read_to_string(egglog_file_path).unwrap();
+        let egraph_info = build_egraph(&egglog_program);
+        let egraph = egraph_info.0;
+        let run_report_matches = egraph
+            .get_run_report()
+            .clone()
+            .unwrap()
+            .num_matches_per_rule
+            .len();
+        assert_eq!(
+            1, run_report_matches,
+            "There should be 1 rule match in program."
+        );
+        assert_eq!(
+            23,
+            egraph.num_tuples(),
+            "There should be 23 facts remaining in the egraph."
+        );
+    }
 }
